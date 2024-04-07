fb22331789237eae02369502d2528d20b77ebf43
Yash <yash@Yashs-MacBook-Pro.local>
2024-04-07 15:27:24 +0530

    feat: added dummy categories and widget

---
+++ lib/screens/categories.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mealy/data/dummy_data.dart';
+import 'package:mealy/widgets/GridItem.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pick your category"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          for (final category in availableCategories)
+            GridItem(category: category)
+        ],
+      ),
+    );
+  }
+}
